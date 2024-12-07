678f177412a95908fab49c2cad47d3ec70fb8bcc
Armando Ruiz <artmx@proton.me>
2024-12-07 14:08:20 -0600

    Practica con switch en fichero switch.js

--- switch.js
+++ switch.js
@@ -1,5 +1,7 @@
-let randomNumber = Math.floor(Math.random() * 101)
-
+// Supongamos que esta variable son las calificaciones de los alumnos de una universidad.
+// En donde se califica del 0 al 100 con valores decimales que, dependiendo de los decimales que sean, se redondea o se truncan, poniendo a varios estudiantes en el mero filo que divide la felicidad y la desgracia.
+// 
+let randomNumber = Math.Math.random() * 101
 switch (randomNumber) {
-    
+    case 
 }
