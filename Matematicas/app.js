73644de065ad88013f3682b296cb172122bca557
Armando Ruiz <artmx@proton.me>
2024-12-26 09:10:34 -0600

    update

--- Matematicas/app.js
+++ Matematicas/app.js
@@ -39,4 +39,39 @@ class TheNavbar extends HTMLElement {
 customElements.define('the-navbar', TheNavbar)
 
 
-/*  */
+/* Componente formulas de algebra (app)*/
+class AlgebraApp extends HTMLElement {
+    constructor(){
+        super();
+    }
+    getTemplate(){
+        const algebraApp = document.createElement('template');
+        algebraApp.innerHTML = `
+        <div>
+            <h1>Algebra Basica</h1>
+            <ol id="app-list">
+                <li>Cuadrado de Binomio</li>
+                <li>Otro tema</li>
+                <li>Otro tema</li>
+                <li>Otro tema</li>
+            </ol>
+        </div>
+        `
+        return algebraApp;
+    }
+    getStyles(){
+        const algebraAppStyles = document.createElement('link');
+        navbarStyles.rel = 'stylesheet';
+        navbarStyles.href = './algebra-app.css';
+        return algebraAppStyles;
+    }
+    render(){
+        document.head.appendChild(this.getStyles())
+        this.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+    connectedCallback(){
+        this.render();
+    }
+}
+
+customElements.define('algebra-app', AlgebraApp)
